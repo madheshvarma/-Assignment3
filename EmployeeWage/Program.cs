dc80bfb8468e305b9d7ebffa7284d6021cdc710d
madheshvarma <madheshvarma369@gmail.com>
2023-02-04 07:45:47 +0530

    yes

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,11 +2,36 @@
 
 namespace EmployeeWage
 {
-    internal class Program
+    public class EmployeeCheck
     {
-        static void Main(string[] args)
+        public static void CheckAttendence()
         {
-            Console.WriteLine("Hello World!");
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_PART_TIME)
+            {
+
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
+
