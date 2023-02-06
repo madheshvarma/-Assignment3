ec276a4b6be9f48fbc88af2260899accea7f445b
madheshvarma <madheshvarma369@gmail.com>
2023-02-06 11:29:32 +0530

    uc1emp

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    public class EmployeeCheck
+    {
+        public static void CheckAttendence()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            EmployeeCheck.CheckAttendence();
+        }
+    }
+}
+
